290cbe0a05335fcd24f66555f34150754d03081a
Your Name <you@example.com>
2019-04-06 00:19:45 -0400

    Entity Framework instalado1234

---
+++ Clientes/Clientes/Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clientes.Models
+{
+    public class Clientes
+    {
+        int ClientesID;
+        string Nombre;
+        double Correo;
+
+    }
+}
